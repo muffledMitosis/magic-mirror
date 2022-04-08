305d29f721154a71e1352a597fa6629d97b56301
Meth Munindradasa <methsndr@gmail.com>
2022-04-08 17:36:32 +0530

    Cleanup

--- main.py
+++ main.py
@@ -4,14 +4,6 @@ import options as opts
 
 from InquirerPy import prompt
 
-# questions = [
-#     {"type": "input", "message": "What's your name:", "name": "name"},
-#     {"type": "confirm", "message": "Confirm?", "name": "confirm"},
-# ]
-# result = prompt(questions)
-# name = result["name"]
-# confirm = result["confirm"]
-
 def main():
     opts.copyOver()
 def cleanup():
